a83ae99912d327f42e1895520ed9d9e35d599c62
Deegoh <tiagopintomonteiro@gmail.com>
2024-12-01 23:32:51 +0100

    Improve folder and file creation

--- src/getInputData.py
+++ src/getInputData.py
@@ -1,13 +1,23 @@
 import requests
 import os
 
-def get_input_data(day):
+def getInputData(day):
 	url = "https://adventofcode.com/2024/day/" + str(day) + "/input"
 	cookies = {'session': os.environ.get('COOKIE_DATA')}
+	folder = "./day" + str(day)
+	path = folder + "/input-day-" + str(day) + ".txt"
+
+	if os.path.exists(folder) is False:
+		os.makedirs(folder)
+		print("folder created")
+
+	if os.path.exists(path):
+		print("file already exists")
+		return
 
 	req = requests.get(url, cookies=cookies)
 	print("input get from day " + str(day))
-	file = open("./input-day-" + str(day) + ".txt", "w")
+	file = open(path, "w")
 	file.write(str(req.text))
 	file.close()
 	print("file created")
