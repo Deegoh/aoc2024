0925b1c74194e919b13186e0b2fd2718bf6e35bc
Deegoh <tiagopintomonteiro@gmail.com>
2024-12-02 19:15:24 +0100

    add solution d2 :/

--- src/day2/day2.py
+++ src/day2/day2.py
@@ -1,4 +1,4 @@
-inputTestPart1="""7 6 4 2 1
+inputTestPart="""7 6 4 2 1
 1 2 7 8 9
 9 7 6 2 1
 1 3 2 4 5
@@ -29,12 +29,16 @@ def part1(data):
 
 
 
-def testPart1():
-	results = part1(inputTestPart1)
-	for i, res in enumerate(resultTestPart1):
+def testPart(input, resultTest, part = 1):
+	result = None
+	if part == 1:
+		results = part1(input)
+	else:
+		results = part2(input)
+	for i, res in enumerate(resultTest):
 		if i >= len(results) or res != results[i]:
 			print("error with index " + str(i))
-			print(inputTestPart1.split("\n")[i])
+			print(input.split("\n")[i])
 			return
 	print("all good")
 
@@ -60,57 +64,70 @@ def checker(row):
 		
 	return True
 
-# def part2(data):
-# 	safeCounter = 0
-# 	rows = data.split("\n")
-# 	results = []
-
-# 	for row in rows:
-# 		row = row.split()
-# 		lock = 0
-# 		sign = 0
-# 		tolerance = 0
-
+def checker2(row, tolerence):
+	sign = 0
+	
+	for i in range(len(row) - 1):
 		
+		differ = int(row[i]) - int(row[i+1])
+		if sign == 0 and differ > 0:
+			sign = 1
+		if sign == 0 and differ < 0:
+			sign = -1
+		if differ > sign and sign == -1:
+			row.remove(row[i])
+			return checker2(row, tolerence + 1)
+		
+		if differ < sign and sign == 1:
+			row.remove(row[i])
+			return checker2(row, tolerence + 1)
+		
+		if abs(differ) > 3 or abs(differ) < 1:
+			row.remove(row[i])
+			return checker2(row, tolerence + 1)
+		
+	return tolerence
 
-# 		# print(row)
-# 		for i, value in enumerate(row):
-# 			if tolerance == 1 and lock == 0:
-# 				lock = 1
-# 				continue
+def part2(data):
+	safeCounter = 0
+	rows = data.strip().split("\n")
+	results = []
 
-# 			if tolerance > 1:
-# 				results.append("Unsafe")
-# 				break
+	for row in rows:
+		print(row, row[0:], row[0+1:])
+		row = row.split(" ")
+		tolerence = 0
+		
+		if checker2(row, tolerence) <= 1:
+			results.append("Safe")
+			safeCounter += 1
+		else:
+			results.append("Unsafe")
 
-# 			if i == len(row) - 1:
-# 				safeCounter += 1
-# 				results.append("Safe")
-# 				break
+	print(safeCounter)
+	return results
 
-# 			# tolerance = checker(value, row[i+1], sign)
-# 			# if tolerance == 1 and i != 0:
-# 			# 	tolerance += checker(row[i-1], row[i+1], sign)
-# 			# 	continue
-
-# 			if i == len(row) - 1:
-# 				safeCounter += 1
-# 				results.append("Safe")
-
-# 	print(safeCounter)
-# 	return results
-
-# def testPart2():
-# 	results = part2(inputTestPart1)
-# 	for i, res in enumerate(resultTestPart2):
-# 		if i >= len(results) or res != results[i]:
-# 			print("error with index " + str(i))
-# 			print(inputTestPart1.split("\n")[i])
-# 			return
-# 	print("all good")
-
-testPart1()
-part1(open("input-day-2.txt", "r").read())
+# testPart(inputTestPart, resultTestPart1)
+# part1(open("input-day-2.txt", "r").read())
 # 598
-# testPart2()
-# part2(open("input-day-2.txt", "r").read())
+testPart(inputTestPart, resultTestPart2, 2)
+# 622 to low
+# 634
+part2(open("input-day-2.txt", "r").read())
+
+
+# def is_safe(row):
+#     inc = [row[i + 1] - row[i] for i in range(len(row) - 1)]
+#     if set(inc) <= {1, 2, 3} or set(inc) <= {-1, -2, -3}:
+#         return True
+#     return False
+
+# with open("input-day-2.txt", "r") as file:
+#     lines = [line.strip() for line in file if line.strip()]
+# data = [[int(y) for y in x.split()] for x in lines]
+
+# safe_count = sum([is_safe(row) for row in data])
+# print(safe_count)
+
+# safe_count = sum([any([is_safe(row[:i] + row[i + 1:]) for i in range(len(row))]) for row in data])
+# print(safe_count)
